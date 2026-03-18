ec60a8f4476b28cb67d41e082638795513c9e2af
a3emond <a3emond@gmail.com>
2026-03-18 09:37:56 -0400

    initial commit / with preliminary assets

---
+++ DeskFortressUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DeskFortressUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DeskFortressUI.Pages")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# Desk Fortress App
